82bf92094c8e0660eb7867f29c877cc71b3dae61
lisuuu96 <adrian.lis1996@gmail.com>
2022-12-28 18:06:08 +0100

    java11

---
+++ js/main8.js
@@ -0,0 +1,7 @@
+let text = 'opis';
+document.querySelector('button').onclick = function()
+   {document.querySelector('p').textContent = text;
+   document.getElementById('opis').textContent = 'opis'}
+
+
+
