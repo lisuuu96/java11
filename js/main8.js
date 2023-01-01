037def7ed4d36cb6f87886028784620e279dbfa8
lisuuu96 <adrian.lis1996@gmail.com>
2023-01-01 18:22:25 +0100

    correct

--- js/main8.js
+++ js/main8.js
@@ -1,7 +1,10 @@
 let text = 'opis';
+
 document.querySelector('button').onclick = function()
-   {document.querySelector('p').textContent = text;
-   document.getElementById('opis').textContent = 'opis'}
+   {
+      document.querySelector('p').textContent = text;
+   }
+   
 
 
 
